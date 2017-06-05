ed47de683757a9554d8df50b600b59bfe84678f6
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-06-04 21:30:57 -0400

    Fix but in RRT path optimizer

---
+++ examples/goals/goals.jl
@@ -0,0 +1,264 @@
+using Gen
+using Distributions
+include("rrt.jl")
+
+immutable PlannerParams
+    rrt_iters::Int
+    rrt_dt::Float64 # the maximum proposal distance
+    refine_iters::Int
+    refine_std::Float64
+end
+
+immutable Path
+    start::Point
+    goal::Point
+    points::Array{Point,1}
+end
+
+function simplify_path(scene::Scene, original::Path)
+    new_points = Array{Point,1}()
+    push!(new_points, original.start)
+    for i=2:length(original.points) - 1
+        if !line_of_site(scene, new_points[end], original.points[i + 1])
+            push!(new_points, original.points[i])
+        end
+    end
+    @assert line_of_site(scene, new_points[end], original.goal)
+    push!(new_points, original.goal)
+    Path(original.start, original.goal, new_points)
+end
+
+function refine_path(scene::Scene, original::Path, iters::Int, std::Float64)
+    # do stochastic optimization
+    new_points = deepcopy(original.points)
+    num_interior_points = length(original.points) -2
+    if num_interior_points == 0
+        return original
+    end
+    for i=1:iters
+        point_idx = 2 + (i % num_interior_points)
+        @assert point_idx > 1 # not start
+        @assert point_idx < length(original.points) # not goal
+        prev_point = new_points[point_idx-1]
+        point = new_points[point_idx]
+        next_point = new_points[point_idx+1]
+        adjusted = Point(point.x + randn() * std, point.y + randn() * std)
+        cur_dist = dist(prev_point, point) + dist(point, next_point)
+        ok_backward = line_of_site(scene, prev_point, adjusted)
+        ok_forward = line_of_site(scene, adjusted, next_point)
+        if ok_backward && ok_forward
+            new_dist = dist(prev_point, adjusted) + dist(adjusted, next_point)
+            if new_dist < cur_dist 
+                # accept the change
+                new_points[point_idx] = adjusted
+            end
+        end
+    end
+    Path(original.start, original.goal, new_points)
+end
+
+function optimize_path(scene::Scene, original::Path, refine_iters::Int, refine_std::Float64)
+    #simplified = simplify_path(scene, original)
+    refined = refine_path(scene, original, refine_iters, refine_std)
+    refined
+end
+
+function plan_path(start::Point, goal::Point, scene::Scene, params::PlannerParams)
+    scheme = HolonomicPointRRTScheme(scene)
+    tree = rrt(scheme, start, params.rrt_iters, params.rrt_dt)
+
+    # find the best path along the tree to the goal, if one exists
+    best_node = tree.nodes[1]
+    min_cost = Inf
+    path_found = false
+    for node in tree.nodes
+        # check for line-of-site to the goal
+        clear_path = line_of_site(scene, node.conf, goal)
+        cost = node.cost_from_start + (clear_path ? dist(node.conf, goal) : Inf)
+        if cost < min_cost
+            path_found = true
+            best_node = node
+            min_cost = cost
+        end
+    end
+
+    local path::Nullable{Path}
+    if path_found
+        # extend the tree to the goal configuration
+        control = Point(goal.x - best_node.conf.x, goal.y - best_node.conf.y)
+        goal_node = add_node!(tree, best_node, goal, control, min_cost)
+        points = Array{Point,1}()
+        node::RRTNode{Point,Point} = goal_node
+        push!(points, node.conf)
+        # the path will contain the start and goal
+        while !isnull(node.parent)
+            node = get(node.parent)
+            push!(points, node.conf)
+        end
+        @assert points[end] == start # the start point
+        @assert points[1] == goal
+        path = Nullable{Path}(Path(start, goal, reverse(points)))
+    else
+        path = Nullable{Path}()
+    end
+    
+    local optimized_path::Nullable{Path}
+    if path_found
+        optimized_path = Nullable{Path}(optimize_path(scene, get(path),
+                                                      params.refine_iters, params.refine_std))
+    else
+        optimized_path = Nullable{Path}()
+    end
+    tree, path, optimized_path # if path is null, then no path was found
+end
+
+function walk_path(path::Path, speed::Float64, times::Array{Float64,1})
+    distances_from_start = Array{Float64,1}(length(path.points))
+    distances_from_start[1] = 0.0
+    for i=2:length(path.points)
+        distances_from_start[i] = distances_from_start[i-1] + dist(path.points[i-1], path.points[i])
+    end
+    locations = Array{Point,1}(length(times))
+    locations[1] = path.points[1]
+    for (time_idx, t) in enumerate(times)
+        if t < 0.0
+            error("times must be positive")
+        end
+        desired_distance = t * speed
+        used_up_time = false
+        # NOTE: can be improved (iterate through path points along with times)
+        for i=2:length(path.points)
+            prev = path.points[i-1]
+            cur = path.points[i]
+            dist_to_prev = dist(prev, cur)
+            if distances_from_start[i] >= desired_distance
+                # we overshot, the location is between i-1 and i
+                overshoot = distances_from_start[i] - desired_distance
+                @assert overshoot <= dist_to_prev
+                past_prev = dist_to_prev - overshoot
+                frac = past_prev / dist_to_prev
+                locations[time_idx] = Point(prev.x * (1. - frac) + cur.x * frac,
+                                     prev.y * (1. - frac) + cur.y * frac)
+                used_up_time = true
+                break
+            end
+        end
+        if !used_up_time
+            # sit at the goal indefinitely
+            locations[time_idx] = path.goal
+        end
+    end
+    locations
+end
+
+function render(path::Path, line_color, start_color, goal_color)
+    plt[:scatter]([path.start.x], [path.start.y], color=start_color, s=200)
+    plt[:scatter]([path.goal.x], [path.goal.y], color=goal_color, s=200)
+    for i=1:length(path.points) - 1
+        a = path.points[i]
+        b = path.points[i + 1]
+        plt[:plot]([a.x, b.x], [a.y, b.y], color=line_color, lw=3, alpha=0.5)
+    end
+end
+
+function planner_demo()
+    # plot them
+    obstacles = []
+    push!(obstacles, Polygon([Point(30, 30), Point(80, 30), Point(80, 35), Point(30, 35)]))
+    push!(obstacles, Polygon([Point(30, 30), Point(30, 80), Point(35, 80), Point(35, 30)]))
+    push!(obstacles, Polygon([Point(60, 30), Point(60, 80), Point(65, 80), Point(65, 30)]))
+    scene = Scene(0, 100, 0, 100, obstacles)
+    start = Point(50, 50)
+    goal = Point(50, 10)
+    @time tree, path, optimized_path = plan_path(start, goal, scene, PlannerParams(1000, 5.0, 1000, 1.0))
+    locations = walk_path(get(optimized_path), 5.0, collect(linspace(0.0, 40.0, 10)))
+    plt[:figure](figsize=(10, 10))
+    render(scene)
+    render(tree)
+    if !isnull(path)
+        render(get(path), "orange", "blue", "red")
+        render(get(optimized_path), "purple", "blue", "red")
+    end
+    plt[:scatter](map((p) -> p.x, locations), map((p) -> p.y, locations), s=100)
+    plt[:savefig]("planner.png")
+end
+#planner_demo()
+
+
+function agent_model(T::Trace, start::Point, planner_params::PlannerParams, 
+                     scene::Scene, speed::Float64, times::Array{Float64,1}, 
+                     measurement_noise::Float64)
+    
+    # sample goal uniformly from scene area
+    goal = Point(uniform(scene.xmin, scene.xmax) ~ "goal_x", 
+                 uniform(scene.xmin, scene.xmax) ~ "goal_y")
+
+    # plan a path from start to goal
+    tree, path, optimized_path = plan_path(start, goal, scene, planner_params)
+
+    if isnull(optimized_path)
+        # no path found
+        fail(T)
+    else
+        # walk the path at a constant speed, and record locations at times
+        locations = walk_path(get(optimized_path), speed, times)
+
+        # add measurement noise to the true locations
+        measurements = Array{Point,1}(length(times))
+        for (i, loc) in enumerate(locations)
+            measurements[i] = Point(normal(loc.x, measurement_noise) ~ "x$i", 
+                                    normal(loc.y, measurement_noise) ~ "y$i")
+        end
+    end
+
+    # return values for debugging and visualization
+    return tree, path, optimized_path, locations, measurements 
+end
+
+function model_demo()
+    # parameters that are fixed for now
+    speed = 10.
+    times = Float64[1, 2, 3, 4, 5]
+    measurement_noise = 1.
+    planner_params = PlannerParams(2000, 3.0, 10000, 1.)
+    obstacles = []
+    push!(obstacles, Wall(Point(20., 40.), 1, 40., 2.))
+    push!(obstacles, Wall(Point(60., 40.), 2, 40., 2.))
+    push!(obstacles, Wall(Point(60.-15., 80.), 1, 15. + 2., 2.))
+    push!(obstacles, Wall(Point(20., 80.), 1, 15., 2.))
+    push!(obstacles, Wall(Point(20., 40.), 2, 40., 2.))
+    scene = Scene(0, 100, 0, 100, obstacles)
+
+    # first simulate some data from the model
+    trace = Trace()
+    start = Point(90., 10.)
+    @time tree, path, optimized_path, locations = agent_model(trace, start, planner_params, scene,
+                                                        speed, times, measurement_noise)
+
+    measured_xs = map((i) -> trace.vals["x$i"], 1:length(times))
+    measured_ys = map((i) -> trace.vals["y$i"], 1:length(times))
+
+    # plot it
+    plt[:figure](figsize=(10, 10))
+    render(scene)
+    render(tree)
+    if !isnull(path)
+        render(get(path), "orange", "blue", "red")
+        render(get(optimized_path), "purple", "blue", "red")
+    end
+    plt[:scatter](map((p) -> p.x, locations), 
+                  map((p) -> p.y, locations), s=100, color="green")
+    plt[:scatter](measured_xs, measured_ys, s=100, color="orange")
+    plt[:savefig]("model.png")
+    
+    
+end
+srand(2)
+model_demo()
+
+
+# TODO it should be possible to extract the value from the trace even if its not random
+# and/or a module. it is just not allowed to be *constrain*ed. it can, however, be named
+# a special subset of random choices that are not constrained, are called 'requested'
+# or 'outputs' and these do have to be modules. still not exactly clear on whether 'outputs' and
+# 'constraints' can coexist in a query
---
+++ examples/goals/rrt.jl
@@ -0,0 +1,260 @@
+
+# --- Generic RRT --------------------
+
+immutable RRTNode{C,U}
+    conf::C
+    # the previous configuration and the control needed to produce this node's
+    # configuration from the previous configuration may be null if it is the
+    # root node
+    parent::Nullable{RRTNode{C,U}}
+    control::Nullable{U}
+    cost_from_start::Float64 # cost (e.g. distance) of moving from start to us
+end
+
+type RRTTree{C,U}
+    nodes::Array{RRTNode{C,U}, 1}
+    function RRTTree(root_conf::C)
+        nodes = Array{RRTNode{C,U},1}()
+        push!(nodes, RRTNode(root_conf, Nullable{RRTNode{C,U}}(), Nullable{U}(), 0.0))
+        new(nodes)
+    end
+end
+
+function add_node!{C,U}(tree::RRTTree{C,U}, parent::RRTNode{C,U}, new_conf::C, control::U, cost_from_start::Float64)
+    node = RRTNode(new_conf, Nullable{RRTNode{C,U}}(parent), Nullable{U}(control), cost_from_start)
+    push!(tree.nodes, node)
+    return node
+end
+
+function root{C,U}(tree::RRTTree{C,U})
+    return tree.nodes[1]
+end
+
+abstract RRTScheme{C,U}
+
+function nearest_neighbor{C,U}(scheme::RRTScheme{C,U}, conf::C,
+                               tree::RRTTree{C,U})
+    nearest::RRTNode{C,U} = root(tree)
+    best_dist::Float64 = dist(nearest.conf, conf)
+    for node::RRTNode{C,U} in tree.nodes
+        d = dist(node.conf, conf)
+        if d < best_dist
+            best_dist = d
+            nearest = node
+        end
+    end
+    nearest
+end
+
+immutable SelectControlResult{C,U}
+    start_conf::C
+    new_conf::C
+    control::U
+    failed::Bool # new_conf is undefined in this case
+    cost::Float64 # cost of this control action (e.g. distance)
+end
+
+function rrt{C,U}(scheme::RRTScheme{C,U}, init::C, iters::Int, dt::Float64)
+    tree = RRTTree{C,U}(init) # init is the root of tree
+    for iter=1:iters
+        rand_conf::C = random_config(scheme)
+        near_node::RRTNode{C,U} = nearest_neighbor(scheme, rand_conf, tree)
+        result = select_control(scheme, rand_conf, near_node.conf, dt)
+        if !result.failed
+            cost_from_start = near_node.cost_from_start + result.cost
+            add_node!(tree, near_node, result.new_conf, result.control, cost_from_start)
+        end
+    end
+    tree
+end
+
+# --- RRT scheme for Holonomic 2D point --------------------
+
+immutable Point
+    x::Float64
+    y::Float64
+end
+
+function dist(a::Point, b::Point)
+    dx = a.x - b.x
+    dy = a.y - b.y
+    sqrt(dx * dx + dy * dy)
+end
+
+abstract Obstacle
+
+immutable Scene
+    xmin::Float64
+    xmax::Float64
+    ymin::Float64
+    ymax::Float64
+    obstacles::Array{Obstacle,1}
+end
+
+immutable Polygon <: Obstacle
+    vertices::Array{Point,1}
+end
+
+function ccw(a::Point, b::Point, c::Point)
+    (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)
+end
+
+function line_intersects_line(a1::Point, a2::Point, b1::Point, b2::Point)
+        # http://algs4.cs.princeton.edu/91primitives/
+        if ccw(a1, a2, b1) * ccw(a1, a2, b2) > 0
+            return false
+        end
+        if ccw(b1, b2, a1) * ccw(b1, b2, a2) > 0
+            return false
+        end
+        return true
+end
+
+function intersects_path(poly::Polygon, path_start::Point, path_end::Point)
+    n = length(poly.vertices)
+    for start_vertex_idx=1:n
+        end_vertex_idx = start_vertex_idx % n + 1 # loop over to 1
+        v1 = poly.vertices[start_vertex_idx]
+        v2 = poly.vertices[end_vertex_idx]
+        if line_intersects_line(v1, v2, path_start, path_end)
+            return true
+        end
+    end
+    return false
+end
+
+function line_of_site(scene::Scene, a::Point, b::Point)
+    for obstacle in scene.obstacles
+        if intersects_path(obstacle, a, b)
+            return false
+        end
+    end
+    return true
+end
+
+using PyCall
+@pyimport matplotlib.path as mplPath
+@pyimport matplotlib.patches as patches
+function render(poly::Polygon)
+    points = map((p) -> Float64[p.x, p.y], poly.vertices)
+    path = mplPath.Path(points)
+    ax = plt[:gca]()
+    patch = patches.PathPatch(path, facecolor="black")
+    ax[:add_patch](patch)
+end
+
+immutable HolonomicPointRRTScheme <: RRTScheme{Point,Point}
+    scene::Scene
+end
+
+function random_config(scheme::HolonomicPointRRTScheme)
+    x = rand() * (scheme.scene.xmax - scheme.scene.xmin) + scheme.scene.xmin
+    y = rand() * (scheme.scene.ymax - scheme.scene.ymin) + scheme.scene.ymin
+    Point(x, y)
+end
+
+
+function select_control(scheme::HolonomicPointRRTScheme, 
+                        target_conf::Point, start_conf::Point, dt::Float64)
+
+
+    dist_to_target = dist(start_conf, target_conf)
+    diff = Point(target_conf.x - start_conf.x, target_conf.y - start_conf.y)
+    distance_to_move = min(dt, dist_to_target)
+    scale = distance_to_move / dist_to_target
+    control = Point(scale * diff.x, scale * diff.y)
+
+    obstacles = scheme.scene.obstacles
+
+    # go in the direction of target_conf from start_conf 
+    new_conf = Point(start_conf.x + control.x, start_conf.y + control.y)
+
+    # test the obstacles
+    failed = false
+    for obstacle in obstacles
+        if intersects_path(obstacle, start_conf, new_conf)
+            # NOTE: could do more intelligent things like backtrack until you succeed
+            failed = true
+            break
+        end
+    end
+    cost = distance_to_move
+    SelectControlResult(start_conf, new_conf, control, failed, cost)
+end
+
+using PyPlot
+function render(scene::Scene)
+    for obstacle in scene.obstacles
+        render(obstacle)
+    end
+    ax = plt[:gca]()
+    ax[:set_xlim](scene.xmin, scene.xmax)
+    ax[:set_ylim](scene.ymin, scene.ymax)
+end
+function render(tree::RRTTree{Point,Point})
+    for node in tree.nodes
+        if !isnull(node.parent)
+            # it is not the root
+            x1 = get(node.parent).conf.x
+            y1 = get(node.parent).conf.y
+            x2 = node.conf.x
+            y2 = node.conf.y
+            plt[:plot]([x1, x2], [y1, y2], color="k")
+        end
+    end
+end
+
+function rrt_demo()
+    # plot them
+    obstacles = [Polygon([Point(30, 30), Point(80, 30), Point(80, 35), Point(30, 35)])] # one tree in the center of hte mpap
+    scene = Scene(0, 100, 0, 100, obstacles)
+    scheme = HolonomicPointRRTScheme(scene)
+    plt[:figure](figsize=(30, 10))
+    for (i, iters) in enumerate([100, 1000, 2000])
+        println("iters: $iters")
+        plt[:subplot](1, 3, i)
+        println("rrt..")
+        @time tree = rrt(scheme, Point(50, 50), iters, 1.)
+        println("rendering..")
+        render(scene)
+        render(tree)
+    end
+    plt[:savefig]("rrt.png")
+end
+#rrt_demo()
+
+
+
+# other obstacle types
+immutable Wall <: Obstacle
+    start::Point
+    orientation::Int # x is 1, y is 2
+    length::Float64
+    thickness::Float64
+    poly::Polygon
+    function Wall(start::Point, orientation::Int, length::Float64,
+                  thickness::Float64)
+        if orientation != 1 && orientation != 2
+            error("orientation must be either 1 (x) or 2 (y)")
+        end
+        vertices = Array{Point,1}(4)
+        vertices[1] = start
+        dx = orientation == 1 ? length : thickness
+        dy = orientation == 2 ? length : thickness
+        vertices[2] = Point(start.x + dx, start.y)
+        vertices[3] = Point(start.x + dx, start.y + dy)
+        vertices[4] = Point(start.x, start.y + dy)
+        poly = Polygon(vertices)
+        new(start, orientation, length, thickness, poly)
+    end
+end
+
+function intersects_path(wall::Wall, path_start::Point, path_end::Point)
+    intersects_path(wall.poly, path_start, path_end)
+end
+
+function render(wall::Wall)
+    render(wall.poly)
+end
+
+
