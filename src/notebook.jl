4b3a392e4afe6697e310845eac31b84293cacef2
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-06-19 11:42:04 -0400

    First reasonable trace rendering API

--- src/notebook.jl
+++ src/notebook.jl
@@ -1,29 +1,43 @@
 import IJulia
 
-type Tile
-    id::String
-    num_cols::Int
-    num_rows::Int
-    width::Int
-    height::Int
-    trace_xmin::Float64
-    trace_ymin::Float64
-    trace_width::Float64
-    trace_height::Float64
-end
-# TODO setters
+global active_viewport = "undefined"
 
-function here(tile::Tile)
+function get_active_viewport()
+    global active_viewport
+    return active_viewport
+end
+
+function random_viewport_name()
+    "id_$(replace(string(Base.Random.uuid4()), "-", ""))"
+end
+
+# TODO: add a global variable that is the 'current viewport' (like current axis in matplotlib)
+# then, there should be a version of here that doesn't require a name, and without explicitly calling attach
+# on the renderer, the renderer should automatically rneder to the current viewport
+# TODO: can this genrealize to other cases?
+# NOTE: the default behavior for a renderer for which attach() has not been called is to render to the active viewport
+# NOTE: the default behavior of here() for which an id has not been given is to generate a rnadom id and set it to the current active viewport.
+# every call to here sets the current active viewport.
+# sub-viewports within viewports are identified by <viewport-id>-i where i ranges from 1 to num_cols * num_rows
+# NOTE: it is also possible to write custom HTML and Javascript that defines
+# custom layouts (the trace rendering just renders a trace to an identified DOM
+# element)
+function here(id::String=random_viewport_name(); num_cols::Int=1, num_rows::Int=1,
+                  width::Int=200, height::Int=200, 
+                  trace_xmin::Real=0.0, trace_ymin::Real=0.0,
+                  trace_width::Real=1.0, trace_height::Real=1.0)
+    global active_viewport = id
+    println("Setting active viewport to $id")
     HTML("""
-<div id="$(tile.id)"></id>
+<svg id="$(id)" width=$(width) height=$(height)></svg>
 <script>
-	var root = document.getElementById("$(tile.id)");
-	var tile_width = $(float(tile.width) / tile.num_cols);
-	var tile_height = $(float(tile.height) / tile.num_rows);
+	var root = document.getElementById("$(id)");
+	var tile_width = $(float(width) / num_cols);
+	var tile_height = $(float(height) / num_rows);
     var i = 1;
-	for (var row=0; row < $(tile.num_rows); row++) {
-		for (var col=0; col < $(tile.num_cols); col++) {
-			var id = "$(tile.id)_" + i;
+	for (var row=0; row < $(num_rows); row++) {
+		for (var col=0; col < $(num_cols); col++) {
+			var id = "$(id)_" + i;
 			var x = tile_width * col;
 			var y = tile_height * row;
 			var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -32,7 +46,7 @@ function here(tile::Tile)
 			svg.setAttribute("y", y);
 			svg.setAttribute("width", tile_width);
 			svg.setAttribute("height", tile_height);
-			svg.setAttribute("viewBox", "$(tile.trace_xmin) $(tile.trace_ymin) $(tile.trace_width) $(tile.trace_height)");
+			svg.setAttribute("viewBox", "$(trace_xmin) $(trace_ymin) $(trace_width) $(trace_height)");
 			root.appendChild(svg);
             i += 1;
 		}
@@ -40,7 +54,6 @@ function here(tile::Tile)
 
 </script>
     """)
-
 end
 
 type JupyterInlineRenderer
@@ -59,23 +72,21 @@ function attach(renderer::JupyterInlineRenderer, dom_element_id::String)
     renderer.dom_element_id = dom_element_id
 end
 
-function viewport(renderer::JupyterInlineRenderer, height::Int=200)
-    renderer.dom_element_id = "id_$(randstring(20))"
-    HTML("""
-<div style="border-style: solid; border-radius: 25px; text-align: center;">
-Trace Rendering ($(renderer.name))
-<div id="$(get(renderer.dom_element_id))" style="height: $(height)px; resize: vertical; position: relative; overflow: hidden; text-align: center;">
-</div>
-</div>
-    """)
-end
-
 function render(renderer::JupyterInlineRenderer, trace::Trace) # TODO handle DifferentiableTrace
+    local id::String
+    global active_viewport
     if (isnull(renderer.dom_element_id))
-        error("No dom element has been defined")
+        if (active_viewport == "undefined")
+            error("No viewport has been defined")
+        end
+        # use global active viewport
+        id = active_viewport
+    else
+        # use explicitly attached viewport
+        id = get(renderer.dom_element_id)
     end
     IJulia.send_comm(renderer.comm, Dict("trace" => trace,
-										 "dom_element_id" => renderer.dom_element_id,
+										 "dom_element_id" => id,
                                          "conf" => renderer.configuration))
 end
 
@@ -91,5 +102,5 @@ export viewport
 export render
 export attach
 
-export Tile
 export here
+export get_active_viewport
