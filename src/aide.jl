59b1cfc6710fd0dc6c27b26c49d95f3fb7efb00c
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-07-22 15:43:07 -0400

    Rename AIDE function and add comment

--- src/aide.jl
+++ src/aide.jl
@@ -1,11 +1,18 @@
-function estimate_elbo(p::AtomicGenerator{T}, p_args::Tuple,
-                       q::AtomicGenerator{T}, q_args::Tuple) where {T}
+"""
+Estimate the KL divergence between two generators with the same output type.
+
+AIDE: An algorithm for measuring the accuracy of probabilistic inference algorithms
+Marco F. Cusumano-Towner, Vikash K. Mansinghka
+https://arxiv.org/abs/1705.07224
+"""
+function aide(p::AtomicGenerator{T}, p_args::Tuple,
+              q::AtomicGenerator{T}, q_args::Tuple) where {T}
     trace = AtomicTrace(T)
     propose!(trace, (), T)
-    (p_log_weight, _) = generate!(p, (p_args), trace)
-    constrain!(trace, (), value(trace, ()))
-    (q_log_weight, ) = generate!(q, (q_args), trace)
+    (p_log_weight, _) = generate!(p, p_args, trace)
+    constrain!(trace, (), trace[()])
+    (q_log_weight, _) = generate!(q, q_args, trace)
     p_log_weight - q_log_weight
 end
 
-export estimate_elbo
+export aide
--- test/aide.jl
+++ test/aide.jl
@@ -1,10 +1,10 @@
 @testset "AIDE" begin
     # crash test
-    est = estimate_elbo(normal, (0, 1), normal, (0, 1))
+    est = aide(normal, (0, 1), normal, (0, 1))
 
     # tiny extreme stochastic test
     # these distributioons are normalized, and kl divergence should be positive
     srand(1)
-    est = mean([estimate_elbo(normal, (0, 1), normal, (2, 1)) for i=1:100])
+    est = mean([aide(normal, (0, 1), normal, (2, 1)) for i=1:100])
     @test est > 0
 end
