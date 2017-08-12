b0ddba0ab03fcb972181926a7ccca375034b3650
Marco Cusumano-Towner <imarcoam@gmail.com>
2017-08-12 18:08:48 -0400

    Fix bug in state space SMC and add unit test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,15 @@
 using Gen
 using Base.Test
 
-#include("ad.jl")
-#include("primitives/primitives.jl")
-#include("math.jl")
-#include("address_trie.jl")
-#include("program.jl")
-#include("nested.jl")
+include("ad.jl")
+include("primitives/primitives.jl")
+include("math.jl")
+include("address_trie.jl")
+include("program.jl")
+include("nested.jl")
 include("replicated.jl")
 include("aide.jl")
-
-
+include("inference/state_space_smc.jl")
 
 #include("sir.jl")
 #include("generator.jl")
